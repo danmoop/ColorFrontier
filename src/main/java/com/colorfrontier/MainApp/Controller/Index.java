db1b71ddfc1e8658b161e9fb685ee98e0e8bd2c0
dan322 <dandurnev0@gmail.com>
2017-12-18 16:25:28 +0300

    Making index page

---
+++ src/main/java/com/colorfrontier/MainApp/Controller/Index.java
@@ -0,0 +1,14 @@
+package com.colorfrontier.MainApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Index
+{
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
---
+++ src/main/java/com/colorfrontier/MainApp/MainAppApplication.java
@@ -0,0 +1,12 @@
+package com.colorfrontier.MainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAppApplication {
+
+	public static void main(String[] args)	 {
+		SpringApplication.run(MainAppApplication.class, args);
+	}
+}
